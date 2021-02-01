de36aecbc6f3580f8eccfd7f36a3d6bec11b60b8
iocaihost <iocaihost@yahoo.com>
2021-01-31 22:32:41 -0500

    modification

--- start.sh
+++ start.sh
@@ -10,11 +10,17 @@
 DATE=`/usr/bin/date +%d-%m-%Y`
 TIME=`/usr/bin/date +%r`
 ##########################
+#    Banner
+##########################
+GUARD=`awk '{print $0}' banner/guard`
+
+##########################
 #    Check privilege
 ##########################
 guard_root(){
 	if [[ ${UID} = "0" ]]; then echo $?
-	elif [[ ${UID} != "0" ]]; then exit 1
+	elif [[ ${UID} != "0" ]]; then clear;
+		printf "\e[5;31m ${GUARD}$USERNAME\n\e[0m"; exit 1
 	fi
 
 }
